aa6b432098118571113b57fe8d13bf6a7078e689
Dhruv <dbpthk@gmail.com>
2026-02-23 17:21:54 +1100

    connect mongoose

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@base-ui/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline">Welcome to To-Do app</Button>;
+    </div>
+  );
+}
---
+++ model/todo.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const TodoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+      maxLength: [100, "Title cannot exceed 100 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+      maxLength: [500, "Description cannot exceed 500 characters"],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
